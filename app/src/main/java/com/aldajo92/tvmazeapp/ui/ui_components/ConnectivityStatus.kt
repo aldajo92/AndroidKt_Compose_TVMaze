0fcea65d24f6e9aa393d835f59cddccc4855cf17
Alejandro Gomez <alejodiscovery20@gmail.com>
2022-07-25 06:12:03 -0500

    removed not used functions

--- app/src/main/java/com/aldajo92/tvmazeapp/ui/ui_components/ConnectivityStatus.kt
+++ app/src/main/java/com/aldajo92/tvmazeapp/ui/ui_components/ConnectivityStatus.kt
@@ -5,35 +5,18 @@ import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
-import com.aldajo92.tvmazeapp.R
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
@@ -54,80 +37,7 @@ fun NoInternetConnectionBar() {
     }
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
-@Preview
-@Composable
-fun NetworkErrorStatus(
-    title: String = "Whoops!",
-    description: String = "No Internet Connection found.\nCheck your connection or try again.",
-    onDismiss: () -> Unit = {}
-) {
-    Dialog(
-        onDismissRequest = onDismiss,
-        properties = DialogProperties(usePlatformDefaultWidth = false),
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = Color.Transparent)
-        ) {
-            Column(
-                modifier = Modifier
-                    .background(
-                        color = MaterialTheme.colors.onPrimary,
-                        shape = RoundedCornerShape(topStart = 5.dp, topEnd = 5.dp)
-                    )
-                    .align(Alignment.BottomCenter),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.bolt_uix_no_internet),
-                    contentDescription = null,
-                    contentScale = ContentScale.FillWidth,
-                    modifier = Modifier.fillMaxWidth()
-                )
-
-                Text(
-                    text = title,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    color = MaterialTheme.colors.onSurface,
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-
-                Text(
-                    text = description,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .padding(top = 10.dp)
-                        .padding(horizontal = 25.dp, vertical = 10.dp)
-                        .fillMaxWidth(),
-                    color = MaterialTheme.colors.onSurface,
-                )
-            }
-
-        }
-    }
-}
-
 // functions taken from this article: https://medium.com/scalereal/observing-live-connectivity-status-in-jetpack-compose-way-f849ce8431c7
-@ExperimentalCoroutinesApi
-@Composable
-fun ConnectivityStatus(
-    Content: @Composable () -> Unit = {},
-    ContentNoConnection: @Composable () -> Unit = {}
-) {
-    val connection by connectivityState()
-    val isConnected = connection === ConnectionState.Available
-
-    if (isConnected) {
-        Content()
-    } else {
-        ContentNoConnection()
-    }
-}
-
 @Composable
 fun connectivityState(): State<ConnectionState> {
     val context = LocalContext.current
