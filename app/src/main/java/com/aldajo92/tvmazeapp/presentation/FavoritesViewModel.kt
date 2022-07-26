59a59a894a52a6896059b18b27cef9bc28066e63
Alejandro Gomez <alejodiscovery20@gmail.com>
2022-07-25 19:54:02 -0500

    get favorites from database

---
+++ app/src/main/java/com/aldajo92/tvmazeapp/presentation/FavoritesViewModel.kt
@@ -0,0 +1,27 @@
+package com.aldajo92.tvmazeapp.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.aldajo92.tvmazeapp.mappers.toUIEvent
+import com.aldajo92.tvmazeapp.repository.favorites.FavoritesRepository
+import com.aldajo92.tvmazeapp.ui.models.ShowResultUIEvents
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+@HiltViewModel
+class FavoritesViewModel @Inject constructor(
+    private val favoritesRepository: FavoritesRepository
+) : ViewModel() {
+
+    val favoritesResultsLiveData: LiveData<ShowResultUIEvents> = favoritesRepository
+        .getFlowData()
+        .map { searchResultStatus ->
+            searchResultStatus.toUIEvent()
+        }
+        .asLiveData()
+
+
+
+}
